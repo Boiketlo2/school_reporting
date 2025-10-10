d678008c3fab564c0ec7f0de9ca7cb27c6db82f5
Boiketlo2 <alotsiboiketlo7@gmail.com>
2025-10-10 20:04:41 +0200

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.REACT_APP_API_BASE || https://school-reporting.onrender.com;
+const API_BASE = process.env.REACT_APP_API_BASE || "https://school-reporting.onrender.com/api";
 
 // --------------------
 // Courses
